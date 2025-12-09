7182f1be16e866ac4385361cc710dfa5cb9d4948
revanth668 <revanth.puppala@iiitb.ac.in>
2025-12-09 09:14:34 +0000

    template code added

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+    //cout<<"working";
+    int t;
+    cin>>t;
+    while(t--){
+        cout<<"working"<<endl;
+        ll n;
+        cin>>n;
+        cout<<n;
+    }
+}
