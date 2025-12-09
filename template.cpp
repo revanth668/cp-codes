81da0b4eaa6f385d7e3a5ceb0b3a17863cafe11a
revanth668 <revanth.puppala@iiitb.ac.in>
2025-12-09 09:26:58 +0000

    template code modified

--- template.cpp
+++ template.cpp
@@ -3,13 +3,9 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-    //cout<<"working";
     int t;
     cin>>t;
     while(t--){
-        cout<<"working"<<endl;
-        ll n;
-        cin>>n;
-        cout<<n;
+        
     }
 }
